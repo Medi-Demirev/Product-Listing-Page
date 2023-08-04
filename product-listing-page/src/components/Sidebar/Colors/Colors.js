edbdb68af8f4f991f3d0d33e981c668ce4161091
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-08-04 17:21:05 +0300

    Refactoring the Color component with dynamic data

--- product-listing-page/src/components/Sidebar/Colors/Colors.js
+++ product-listing-page/src/components/Sidebar/Colors/Colors.js
@@ -1,10 +1,53 @@
 import React from 'react';
 
 import './Colors.css'
+import Input from '../../Input/Input';
 
-function Colors() {
+function Colors({handleChange}) {
   return (
-    <div>Colors</div>
+    <>
+    <div>
+    <h2 className='sidebar-title color-title'>Colors</h2>
+    <label className='sidebar-label-container'>
+        <input type='radio' onChange={handleChange} value= "" name='test3'></input>
+        <span className='checkmark' style={{background:"linear-gradient(blue,crimson)"}}></span> All
+    </label>
+    <Input
+    handleChange={handleChange}
+    value={'black'}
+    title={'Black'}
+    name={'test3'}
+    color={'black'}
+    />
+    <Input
+    handleChange={handleChange}
+    value={'blue'}
+    title={'Blue'}
+    name={'test3'}
+    color={'blue'}
+    />
+    <Input
+    handleChange={handleChange}
+    value={'red'}
+    title={'Red'}
+    name={'test3'}
+    color={'red'}
+    />
+    <Input
+    handleChange={handleChange}
+    value={'green'}
+    title={'Green'}
+    name={'test3'}
+    color={'green'}
+    />
+    <label className='sidebar-label-container'>
+        <input type='radio' onChange={handleChange} value= "white" name='test3'></input>
+        <span className='checkmark' style={{background:'white', border:'1px solid black'}}></span> White
+    </label>
+    </div>
+    </>
+
+    
   )
 }
 
