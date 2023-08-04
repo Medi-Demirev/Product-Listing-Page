658ac111904bb4246340df54f5f71126322a9897
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-08-04 17:22:53 +0300

    Refactoring the Recommended component with dynamic data

--- product-listing-page/src/components/Card/Card.js
+++ product-listing-page/src/components/Card/Card.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import {BsFillBagHeartFill} from 'react-icons/bs';
 
-function Card() {
+const Card =({img,title,star,reviews,prevPrice,newPrice})=> {
   return (
-    <div>Card</div>
+    <section className='card'>
+            <img className='card-img' src={img} alt={title}/>
+            <div className='card-details'>
+            <h3 className='card-title'>{title}</h3>
+            <section className='card-reviews'>
+            {star} {star} {star} {star}
+            <span className='total-reviews'>{reviews}</span>
+            </section>
+            <section className='card-price'>
+                <div className='price'>
+                    <del>{prevPrice}</del> ${newPrice}
+                </div>
+                <div className='bag'>
+                <BsFillBagHeartFill className='bag-icon'/>
+                </div>
+
+            </section>
+
+            </div>
+        </section>
   )
 }
 
