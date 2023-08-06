deaba9c209ea8dbeb87c60dc84e26f2f11b8ec5c
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-08-07 00:58:41 +0300

    Fix errors

--- product-listing-page/src/components/Sidebar/Sidebar.js
+++ product-listing-page/src/components/Sidebar/Sidebar.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Category from './Category/Category';
 import Price from './Price/Price';
 import Colors from './Colors/Colors';
-
+import logo from "../../assets/logo.png"
 import './Sidebar.css'
 
 const Sidebar = ({handleChange}) =>{
   return (
     <section className='sidebar'>
+      <div className='logo-container'>
+            <img className='logo-img' src={logo} alt='logo'/>
+        </div>
         <Category handleChange={handleChange}/>
         <Price handleChange={handleChange}/>
         <Colors handleChange={handleChange}/>
