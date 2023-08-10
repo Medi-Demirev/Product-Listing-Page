0eb38e28ee88f4f714f1ba72ff1348d1049936c3
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-08-11 01:04:05 +0300

    Delete Sort menu component

--- product-listing-page/src/components/Sort/Sort_menu.js
+++ product-listing-page/src/components/Sort/Sort_menu.js
@@ -5,15 +5,7 @@ import './Sort_menu.css'
 const Sort_menu = ({handleSortChange}) => {
   return (
   <>
- <div>
-<h2 className="sort-title">Sort By</h2>
-<select onChange={handleSortChange} className='sorting-container'>
-    <option  className='sort-btns' value="lowtohigh">Price: Low to High </option>
-    <option  className='sort-btns' value="hightolow">Price: High to Low </option>
-    <option  className='sort-btns' value="AtoZ">Alphabetical: A-Z </option>
-    <option  className='sort-btns' value="ZtoA">Alphabetical: Z-A </option>
-</select>
-</div>
+ 
 </>
   )
 }
