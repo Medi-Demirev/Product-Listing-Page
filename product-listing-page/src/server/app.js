9d951f3e65b6397f87ea811f9ff7f7aefef93254
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2023-08-13 16:31:51 +0300

    Create new route and add page functionality

--- product-listing-page/src/server/app.js
+++ product-listing-page/src/server/app.js
@@ -9,14 +9,31 @@ let corsOptions = {
     origin : ['http://localhost:3000/'],
  }
    
- app.use(cors(corsOptions))
+ app.use(cors(corsOptions));
 
-app.get("/api", (req, res) => {
+ app.get("/Allproducts", (req, res) => {
+   
     res.header(
-      {  "Access-Control-Allow-Origin" : "*",  
+        {  "Access-Control-Allow-Origin" : "*",  
         "Access-Control-Allow-Credentials"  : "true"}
     )
    res.json(data)
+});
+
+app.get("/products", (req, res) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page -1) * limit;
+    const endIndex = page * limit; 
+  
+const result = data.slice(startIndex, endIndex)
+
+    res.header(
+        {  "Access-Control-Allow-Origin" : "*",  
+        "Access-Control-Allow-Credentials"  : "true"}
+    )
+   res.json(result)
 });
 
 app.listen(5000, ()=> {console.log('Server started on port 5000');})
